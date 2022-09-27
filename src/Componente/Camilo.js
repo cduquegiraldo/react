653e428d14998370ce4b7d7b353031bba4d6f028
407 <407@academicos.cesde.edu.co>
2022-09-27 17:35:27 -0500

    se crea app de react

---
+++ src/Componente/Camilo.js
@@ -0,0 +1,11 @@
+export function Camilo (){
+    let nombre='camilo'
+    return(
+        <>
+            <h1>HOLA REACT SON {nombre}</h1>
+            <hr/>
+            
+        </>
+        
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Banner } from './Componente/Banner.js';
+import {Camilo} from './Componente/Camilo.js';
+import { Menu } from './Componente/Menu.js';
+import { Sinopsis } from './sinopsis/Sinopsis.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Menu></Menu>
+    <Banner/>
+    <Sinopsis/>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
